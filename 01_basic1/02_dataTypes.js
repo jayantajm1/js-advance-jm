94ec4635c27fb3c4e2fb044e64146c9a1917204d
Jayanta Mardi <90429653+jayantajm1@users.noreply.github.com>
2023-09-25 16:05:13 +0000

    02_data types

---
+++ 01_basic1/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version;
+// alert("i lv ew") cant work cz we are using nodejs not browser
+// to know about js sentext go on  alert mdn
+let name="Jayanta"
+let age=21
+let isLoggedIn=false;
+let state;
+/* number => 2 to power 53
+bigint = large number block trending
+string=> ""
+boolean=> true/false
+null standardlone value
+undefine =>mean values is not asign yet
+symbol=> for find uniqueness
+object=>
+
+
+*/
+console.log(typeof "jayanta")
+console.log(typeof null)
+console.log(typeof undefine)
+
+
