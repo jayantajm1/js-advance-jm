458e428b0d45944d56d62fd0c53e9fcf688262e7
Jayanta Mardi <90429653+jayantajm1@users.noreply.github.com>
2023-09-25 15:52:08 +0000

    01_variable added

---
+++ 01_basic1/01_varibles.js
@@ -0,0 +1,14 @@
+const accountId =82676221
+let accountEmail="jaya@gmail.com"
+var accountPassword="3288212"
+accountCity="kolkata"
+// accountId=8271761 const values can not changed
+
+console.log(accountId)
+/* instate of var we use let bcz var can not block scope or varible scope
+*/
+accountEmail="maya@gmail.com"
+accountPassword="9862876"
+accountCity="malda"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
