d061642a59f74f433b0c3ce391577e13b6beb8a2
Jayanta Mardi <90429653+jayantajm1@users.noreply.github.com>
2023-09-25 16:23:12 +0000

    03_coversionOperations

---
+++ 01_basic1/03_conversionOperations.js
@@ -0,0 +1,23 @@
+/* when work at backend and  we don't know the type of the values then we need to type conversion
+*/
+let score=null
+let valueInNumber=Number(score)
+console.log(typeof score)
+console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+/*  score =undefine ,"jaua" mean o/p o will be NaN => special  type of data
+// "33"=> 33
+"33abc"=>NaN
+true=>1
+false=>0
+*/
+let loggedIn="jaya"
+let booleanLoggedIn=Boolean(loggedIn)
+console.log(booleanLoggedIn)
+/* 1=>true; 0=> false
+""=>false
+"jaa"=>true
+*/
+let j=76
+let stringNum= String(j)
+console.log(typeof stringNum)
