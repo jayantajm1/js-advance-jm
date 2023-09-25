8f057980e93198872fb6954155aac9fd04c0e12b
Jayanta Mardi <90429653+jayantajm1@users.noreply.github.com>
2023-09-25 17:56:23 +0000

    operations

--- 01_basic1/03_conversionOperations.js
+++ 01_basic1/03_conversionOperations.js
@@ -21,3 +21,32 @@ console.log(booleanLoggedIn)
 let j=76
 let stringNum= String(j)
 console.log(typeof stringNum)
+
+
+//*********Operations **********
+/*let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2**3)
+console.log(2%3)
+*/
+let str1="hello"
+let str2= "world"
+console.log(str1+ str2)
+console.log("2"+2)
+console.log(2+"2")
+console.log("1"+2 +2)
+console.log(1+2+"2") // here atfirst it add the first two number then its perform string operation (ecmacscript for more info)
+//console.log(3+5*2%2);
+//console.log(+true)
+let gameCounter=100;
+gameCounter++
+console.log(gameCounter)
+let x=3
+const y=x++
+// expected op x:4 y:3
+console.log(x,y)
+let a=3;
+const b=++a
+console.log(a,b)
+//expected op a:4 b:4
